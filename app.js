02a504f3424e312552e8106bd974709fb7f8a4e3
David Francisco <dfrancisco2677@gmail.com>
2022-03-22 19:01:18 -0700

    added some js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+let myName = 'David';
+
+// eslint-disable-next-line no-console
+console.log (`Hello ${myName}`);
+
+const button = document.getElementById('button');
+button.addEventListener('click', ()=>{
+//store the element with id email-inputs
+    // add the class "hide" to the element
+// with an id of email-inputs
+// remove the class "hide" from the element with id thank-you
+//
+});
+
+
+
+
+
 // import functions
 
 // grab DOM elements
